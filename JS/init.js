7bada3814ccc7fde54a52261b0afcfa06232865b
Eneko Bikandi <ebikandi001@gmail.com>
2016-09-24 00:42:02 +0200

    [init.js]: New version

--- JS/init.js
+++ JS/init.js
@@ -9,9 +9,9 @@
           return re.test(email);
     }
 
-    //Credit: https://goo.gl/QuVJ2s (Modified UK Phone Number)
+    //Credit: https://goo.gl/QuVJ2s (UK Mobile Phone Number)
     function validatePhoneNumber(phoneNumber) {
-          var re = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
+          var re = /^(\+44?7\d{3}|\(?07\d{3}\)?)?\d{3}?\d{3}$/;
           return re.test(phoneNumber);
     }
     
@@ -39,63 +39,46 @@
         }
     }
     
+    function callReportMethod(map, elem){
+        switch(map.attribute){
+                 case 'text':
+                    reportTextOrValue(map.id, elem.innerHTML, map.isEmail, map.isPhoneNumber ); 
+                    break;
+                case 'value':
+                    reportTextOrValue(map.id, elem.value, map.isEmail, map.isPhoneNumber );
+                    break;                        
+                case 'radio':
+                    reportRadio(map.id, elem.value, elem.checked);                   
+                    break;
+                case 'checkbox':
+                    reportCheckbox(map.id, elem.checked);                            
+                break;
+        }
+    }
+    
 	/**
 	 * This function is responsible for capturing data (based on mappings.js) and sending data (with use of DataReporter class).
 	 *
 	 * @name init
 	 */
 	function init () {
-    
         mappings.forEach((map)=>{
-            //@TODO: Parse selector to use concrete method instead of querySelectorAll() to gain speed
+            //@TODO: Parse selector to use concrete methods instead of querySelectorAll() to gain speed
             var elements = document.querySelectorAll(map.selector);
             var parsedEvent = map.event.replace("on",'').toLowerCase();
             
             //There always can be multiple element with the same selector
             elements.forEach((elem)=>{
                 var isOnLoad = parsedEvent === 'load';
-                switch(map.attribute){
-                        case 'text':
-                            if(isOnLoad){
-                                reportTextOrValue(map.id, elem.innerHTML, map.isEmail, map.isPhoneNumber );
-                            } else {
-                                elem.addEventListener(parsedEvent, function(){
-                                    reportTextOrValue(map.id, elem.innerHTML, map.isEmail, map.isPhoneNumber);
-                                });
-                            }
-                            break;
-                        case 'value':
-                            if(isOnLoad){
-                                reportTextOrValue(map.id, elem.value, map.isEmail, map.isPhoneNumber );
-                            } else {
-                                elem.addEventListener(parsedEvent, function(){
-                                    reportTextOrValue(map.id, elem.value, map.isEmail, map.isPhoneNumber);
-                                });
-                            }
-                            break;                        
-                        case 'radio':
-                            if(isOnLoad){
-                                reportRadio(map.id, elem.value, elem.checked);
-                            } else {
-                                elem.addEventListener(parsedEvent, function(){
-                                    reportRadio(map.id, elem.value, elem.checked);
-                                });
-                            }                        
-                            break;
-                        case 'checkbox':
-                            if(isOnLoad){
-                                reportCheckbox(map.id, elem.checked);
-                            } else {
-                                elem.addEventListener(parsedEvent, function(){
-                                    reportCheckbox(map.id, elem.checked);
-                                });
-                            }                               
-                        break;
-                }                 
+                if(isOnLoad){
+                    callReportMethod(map, elem);
+                } else {
+                    elem.addEventListener(parsedEvent, function(){
+                        callReportMethod(map, elem);
+                    }, false);
+                }     
             });
         });
-        
-        alert("so far so good");
 	}
     
 	document.addEventListener('DOMContentLoaded', init, false);
