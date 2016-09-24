dcdd58c470dd880cb52ea32fb736d7151ad51135
Eneko Bikandi <ebikandi001@gmail.com>
2016-09-24 07:54:10 +0200

    Merge logic to Mapper class in order to make it's code reusable and init() more readable

---
+++ JS/Classes/Mapper.js
@@ -0,0 +1,91 @@
+(function(){
+    'use strict';
+    
+    
+        function Mapper(){
+            this.dataReporter = new classes.DataReporter();
+        }
+        
+       //Credit: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+        Mapper.prototype.validateEmail = function(email) {
+              var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              return re.test(email);
+        }
+        
+
+        //Credit: https://goo.gl/QuVJ2s (UK Mobile Phone Number)
+        //As the input is type=number we don't accept spaces in the regex
+        Mapper.prototype.validatePhoneNumber = function(phoneNumber) {
+              var re = /^(\+44?7\d{3}|\(?07\d{3}\)?)?\d{3}?\d{3}$/;
+              return re.test(phoneNumber);
+        }
+
+        Mapper.prototype.reportTextOrValue = function(id, data, isEmail, isPhoneNumber){
+            if(isEmail === true && validateEmail(data) === false){
+                return 0;
+            } else if(isPhoneNumber === true && validatePhoneNumber(data) === false){
+                return 0;
+            } else{
+                this.dataReporter.send(id, data);
+            }
+        };
+
+        Mapper.prototype.reportRadio = function(id, data, isChecked){
+            if(isChecked === true){
+                this.dataReporter.send(id, data);
+            }
+        }
+
+        Mapper.prototype.reportCheckbox = function(id, isChecked){
+            if(isChecked === true){
+                this.dataReporter.send(id, "Checked");
+            } else{
+               this.dataReporter.send(id, "Unchecked"); 
+            }
+        }
+        
+        Mapper.prototype.callReportMethod = function(map, elem){
+            switch(map.attribute){
+                     case 'text':
+                        this.reportTextOrValue(map.id, elem.innerHTML, map.isEmail, map.isPhoneNumber ); 
+                        break;
+                    case 'value':
+                        this.reportTextOrValue(map.id, elem.value, map.isEmail, map.isPhoneNumber );
+                        break;                        
+                    case 'radio':
+                        this.reportRadio(map.id, elem.value, elem.checked);                   
+                        break;
+                    case 'checkbox':
+                        this.reportCheckbox(map.id, elem.checked);                            
+                    break;
+            }
+        }
+        
+        Mapper.prototype.mapAndReport = function(){
+            mappings.forEach((map)=>{
+            
+            //@TODO: Parse selector to use concrete methods instead of querySelectorAll() to gain speed
+            var elements = document.querySelectorAll(map.selector);
+            
+            //Parse event
+            var parsedEvent = map.event.replace("on",'').toLowerCase();
+            
+            //There always can be multiple element with the same selector
+            elements.forEach((elem)=>{
+                var isOnLoad = parsedEvent === 'load';
+                if(isOnLoad){
+                    this.callReportMethod(map, elem);
+                } else {
+                    elem.addEventListener(parsedEvent, function(){
+                        this.callReportMethod(map, elem);
+                    }, false);
+                }     
+            });
+        
+        });
+        }
+    
+ 
+    classes.Mapper = Mapper;
+    
+}());
--- JS/init.js
+++ JS/init.js
@@ -1,86 +1,20 @@
-(function () {
+(function() {
 	'use strict';
 
-    var dataReporter = new classes.DataReporter();        
-
-     //Credit: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
-    function validateEmail(email) {
-          var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return re.test(email);
-    }
-
-    //Credit: https://goo.gl/QuVJ2s (UK Mobile Phone Number)
-    function validatePhoneNumber(phoneNumber) {
-          var re = /^(\+44?7\d{3}|\(?07\d{3}\)?)?\d{3}?\d{3}$/;
-          return re.test(phoneNumber);
-    }
-    
-    function reportTextOrValue(id, data, isEmail, isPhoneNumber){
-        if(isEmail === true && validateEmail(data) === false){
-            return 0;
-        } else if(isPhoneNumber === true && validatePhoneNumber(data) === false){
-            return 0;
-        } else{
-            dataReporter.send(id, data);
-        }
-    };
-    
-    function reportRadio(id, data, isChecked){
-        if(isChecked === true){
-            dataReporter.send(id, data);
-        }
-    }
-   
-    function reportCheckbox(id, isChecked){
-        if(isChecked === true){
-            dataReporter.send(id, "Checked");
-        } else{
-           dataReporter.send(id, "Unchecked"); 
-        }
-    }
-    
-    function callReportMethod(map, elem){
-        switch(map.attribute){
-                 case 'text':
-                    reportTextOrValue(map.id, elem.innerHTML, map.isEmail, map.isPhoneNumber ); 
-                    break;
-                case 'value':
-                    reportTextOrValue(map.id, elem.value, map.isEmail, map.isPhoneNumber );
-                    break;                        
-                case 'radio':
-                    reportRadio(map.id, elem.value, elem.checked);                   
-                    break;
-                case 'checkbox':
-                    reportCheckbox(map.id, elem.checked);                            
-                break;
-        }
-    }
-    
 	/**
 	 * This function is responsible for capturing data (based on mappings.js) and sending data (with use of DataReporter class).
 	 *
 	 * @name init
 	 */
 	function init () {
-        mappings.forEach((map)=>{
-            //@TODO: Parse selector to use concrete methods instead of querySelectorAll() to gain speed
-            var elements = document.querySelectorAll(map.selector);
-            var parsedEvent = map.event.replace("on",'').toLowerCase();
-            
-            //There always can be multiple element with the same selector
-            elements.forEach((elem)=>{
-                var isOnLoad = parsedEvent === 'load';
-                if(isOnLoad){
-                    callReportMethod(map, elem);
-                } else {
-                    elem.addEventListener(parsedEvent, function(){
-                        callReportMethod(map, elem);
-                    }, false);
-                }     
-            });
-        });
+        var Mapper = classes.Mapper,
+                mapperInstance;
+        
+        mapperInstance = new Mapper();
+        
+        mapperInstance.mapAndReport();    
 	}
     
 	document.addEventListener('DOMContentLoaded', init, false);
-
+    
 }());
